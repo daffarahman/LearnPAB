4f7058206825d0057ed3984226a1057676f6309a
daffarahman <69158099+daffarahman@users.noreply.github.com>
2026-02-04 11:22:46 +0700

    learn: add button and onClick

--- app/src/main/java/com/madebydap/learnpab/MainActivity.kt
+++ app/src/main/java/com/madebydap/learnpab/MainActivity.kt
@@ -5,26 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Popup
 import com.madebydap.learnpab.ui.theme.LearnPABTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,75 +37,84 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LearnPABTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    GreetingImage(
-                        stringResource(R.string.happy_birthday_text),
-                        stringResource(R.string.signature_text),
-                        modifier = Modifier.padding(8.dp)
-                    )
-                }
+                RegistrationForm()
             }
         }
     }
 }
 
 @Composable
-fun GreetingText(message: String, sender: String, modifier: Modifier = Modifier) {
-    var count = remember {
-        mutableStateOf(0)
+fun RegistrationForm() {
+    var name = remember {
+        mutableStateOf("")
     }
+    var showDialog = remember {
+        mutableStateOf(false)
+    }
+    Image(
+        painter = painterResource(R.drawable.androidparty),
+        contentScale = ContentScale.Crop,
+        contentDescription = "Android Party"
+    )
     Column(
-        verticalArrangement = Arrangement.Center,
-        modifier = modifier
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
     ) {
-        Text(
-            text = message,
-            fontSize = 100.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
-        )
-        Text(
-            text = "From $sender",
-            fontSize = 36.sp,
+        Surface (
+            color = Color.Blue,
             modifier = Modifier
-                .padding(16.dp)
-                .align(Alignment.CenterHorizontally)
-        )
-        Text(
-            text = count.value.toString(),
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-        )
-        Button(
-            onClick = {
-                count.value++
-            },
-            modifier = Modifier.align(Alignment.CenterHorizontally)
+                .fillMaxWidth()
         ) {
-            Text(text = "Click Me!")
+            Text(
+                text = "Birthday Present Form",
+                color = Color.White
+            )
         }
-    }
-}
-
-@Composable
-fun GreetingImage(message: String, sender: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.androidparty)
-    Box {
-        Image(
-            painter = image,
-            contentDescription = null,
-            contentScale = ContentScale.Crop,
-            alpha = 0.5F
-        )
-        GreetingText(
-            message = message,
-            sender = sender,
+        // First form
+        Row(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
-        )
+                .fillMaxWidth()
+        ) {
+            Text(
+                text = "Name: "
+            )
+            OutlinedTextField(
+                value = name.value,
+                onValueChange = { text ->
+                    name.value = text
+                }
+            )
+        }
+        Button (
+            onClick = {
+                showDialog.value = true
+            },
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        ) {
+            Text(text = "Submit!")
+        }
+
+        if (showDialog.value) {
+            Popup(
+                alignment = Alignment.Center,
+                onDismissRequest = {
+                    showDialog.value = false
+                }
+            ) {
+                Column {
+                    Text(name.value)
+                    Button(
+                        onClick = {
+                            showDialog.value = false
+                        }
+                    ) {
+                        Text("Ok!")
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -109,6 +122,6 @@ fun GreetingImage(message: String, sender: String, modifier: Modifier = Modifier
 @Composable
 fun LearnPABPreview() {
     LearnPABTheme {
-        GreetingImage("Happy Birthday Dap!", "Rivan")
+        RegistrationForm()
     }
 }
